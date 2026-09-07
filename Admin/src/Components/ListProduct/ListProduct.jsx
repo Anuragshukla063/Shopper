c57f5fe493f04e07a56a8e50c9ecd65adce52a16
Anurag Shukla <nikhilshukla346@gmail.com>
2026-09-07 23:06:26 +0530

    Intial commit

---
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
+const ListProduct = () => {
+  const [allproducts, setAllProducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts", { cache: "no-store" })
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+  const remove_product = async (id) => {
+    await fetch("http://localhost:4000/removeproducts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    });
+    const data = await res.json();
+    console.log("Remove response:", data);
+    await fetchInfo();
+  };
+
+  return (
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="list-product-format-main listproduct-format"
+              >
+                <img
+                  src={product.image}
+                  alt=""
+                  className="listproduct-product"
+                />
+                <p>{product.name}</p>
+                <p>${product.old_price}</p>
+                <p>${product.new_price}</p>
+                <p>{product.category}</p>
+                <img
+                  src={cross_icon}
+                  onClick={() => {
+                    remove_product(product.id);
+                  }}
+                  alt=""
+                  className="listproduct-remove-icon"
+                />
+              </div>
+              <hr />
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ListProduct;
